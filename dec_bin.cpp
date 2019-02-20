071e9fe8f433e9443c5554ef68041298b35dd731
VardanYordanyan <yordanyanvardan@gmail.com>
2019-02-20 14:45:30 +0400

    dec_bin.cpp patrast handznelu

--- dec_bin.cpp
+++ dec_bin.cpp
@@ -8,25 +8,21 @@ int get_int()
     int result = 0;
     std::string s("");
     do{
-        std::cout << "Input the number... " << std::endl;
+        std::cout << "Input decimal number... " << std::endl;
         getline (std::cin, s);
         fl = false;
-        if (s[0] == '-') {
-            for (int i = 1; i < s.size(); ++i) {
-                if (s[i] < '0' || s[i] > '9') {
-                    std::cout << "Error input ... " << s[i] << std::endl;
-                    fl = true;
-                    break;
-                }
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i] < '0' || s[i] > '9') {
+                std::cout << "Error input ... " << s[i] << std::endl;
+                fl = true;
+                break;
             }
         }
-        else {
-            for (int i = 0; i < s.size(); ++i) {
-                if (s[i] < '0' || s[i] > '9') {
-                    std::cout << "Error input ... " << s[i] << std::endl;
-                    fl = true;
-                    break;
-                }
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] < '0' || s[i] > '9') {
+                std::cout << "Error input ... " << s[i] << std::endl;
+                fl = true;
+                break;
             }
         }
     } while (fl);
@@ -35,31 +31,22 @@ int get_int()
     return result;
 }
 
-int dec_bin ()
-{	int dec = get_int();
-	int bin[10];
-    int j = 1;
-	while (dec != 0) {
-		for (int i = 10; i > 0; --i) {
-            bin[i] = dec % 2;
-        }
-        
-            dec = dec / 2;
-            std::cout << bin[j];
-            ++j;
-        
-        
-        
-        /*int i = 100;
-		i = i - 1;
+void dec_bin ()
+{	//This function returns the decimal numbers with binar numbers
+    int dec = get_int();
+    int bin[100] = {};
+    int j = 0;
+    std::cout << "binar number ..." << std::endl; 
+    for (int i = 99; i > 0; --i) {	
+        if (dec == 0) {
+            break;}
         bin[i] = dec % 2;
+        ++j;
         dec = dec / 2;
-	//	std::cout << bin[i];
-	}
-    for (int j = 1; j < 100; ++j) {
-    
-		std::cout << bin[j];*/
-    } 
+        }
+    for (int i = (100 - j); 100 > i; ++i){
+    std::cout << bin[i];}
+    std::cout << std::endl;
 }
 
 int main ()
